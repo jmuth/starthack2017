7e81e27539d395ff5a0849177449826535bbf0f0
speierers <sebastien.speierer@epfl.ch>
2017-03-18 16:01:46 +0100

    add middle point spline

--- main.py
+++ main.py
@@ -1,60 +1,74 @@
 from path import *
-from screenshot import *
-from video import *
+#from screenshot import *
+#from video import *
 #from sights import get_sights
 
 if __name__ == '__main__':
-	#get_sights('London')
+    #get_sights('London')
 
-	p_eiffel = (48.8584, 2.2945, 60)
-	p_triomphe = (48.8738, 2.2950, 60)
-	p_chaillot = (48.8620159,2.2878386, 80)
-	p_grand_palais = (48.8663031,2.3127906, 80)
-	p_louvre = (48.8612266,2.3357741, 80)
-	p_monmartre = (48.8866677,2.3430436, 120)
+    # paris
+    p_eiffel = (48.8584, 2.2945, 60)
+    p_triomphe = (48.8738, 2.2950, 60)
+    p_chaillot = (48.8620159,2.2878386, 80)
+    p_grand_palais = (48.8663031,2.3127906, 80)
+    p_louvre = (48.8612266,2.3357741, 80)
+    p_monmartre = (48.8866677,2.3430436, 120)
 
-	path = [[],[],[],[],[],[]]
-
-	path = plan_trip((p_eiffel, p_triomphe, p_chaillot, p_grand_palais, p_louvre, p_monmartre))
-
-	# ensure that h are in monotically decreasing
-	for x in range(1,len(path[4])):
-		if path[4][x] > path[4][x-1]:
-			path[4][x] = path[4][x] - 360.0
-
-	#for i in range(len(path[0])):
-	#	print("https://www.google.ch/maps/" + point_to_string(path_at(path, i)) + "t/data=!3m1!1e3")
-
-	#print([int(x) for x in path[4]])
-	interpolated_path = spline_interpolation(path, 35)
-
-	print("+++++++ Interpolated path +++++++")
-
-	for i in range(len(interpolated_path[0])):
-		screenshot_url("https://www.google.ch/maps/" + point_to_string(path_at(interpolated_path, i)) + "t/data=!3m1!1e3", i+1)
-
-	images_to_video('out/', '.png', 'path.mp4')
-
-#	print([int(x) for x in interpolated_path[4]])
-
-	'''
-	import matplotlib.pyplot as plt
-	ax = plt.axes()
-	ax.plot(path[0], path[1], 'o', interpolated_path[0], interpolated_path[1], '-')
-
-	#d_x = [sin(h * 2.0 * pi / 360 + pi / 4.0) * 0.003 for h in path[4]]
-	#d_y = [cos(h * 2.0 * pi / 360 + pi / 4.0) * 0.003 for h in path[4]]
-
-	#for i in range(len(path[0])):
-	#	plt.arrow(path[0][i], path[1][i], d_x[i], d_y[i], fc="k", ec="k", head_width=0.001, head_length=0.001 )
-		#plt.arrow(interpolated_path[0][i], interpolated_path[1][i], d_x[i], d_y[i], fc="k", ec="k", head_width=0.00001, head_length=0.001 )
+    # fribourg
+    p_poya = (46.8133523,7.1645176, 60)
+    p_cathedral = (46.8062408,7.1629309, 60)
+    p_st_michel = (46.8066934,7.157652, 60)
+    p_perolle = (46.7940545,7.1470015, 60)
 
 
-	#ax.axis([48.84, 48.9, 2.28, 2.32])
-	#ax.arrow( 1.5, 0.8, 0.2, -0.2, fc="k", ec="k", head_width=0.05, head_length=0.1 )
+    path = [[],[],[],[],[],[]]
 
-	plt.show()
-	print(interpolated_path[4])
-	'''
+    #path = plan_trip((p_eiffel, p_triomphe, p_chaillot, p_grand_palais, p_louvre, p_monmartre))
+    #path = plan_trip((p_eiffel, p_louvre, p_monmartre, p_chaillot, p_grand_palais))
+    #path = plan_trip((p_poya, p_cathedral, p_st_michel, p_perolle), 400)
+    path = plan_trip((p_perolle, p_poya, p_cathedral), 500)
+    # ensure that h are in monotically decreasing
+    for x in range(1,len(path[4])):
+        if path[4][x] > path[4][x-1]:
+            path[4][x] = path[4][x] - 360.0
+
+    for i in range(len(path[0])):
+       print("https://www.google.ch/maps/" + point_to_string(path_at(path, i)) + "t/data=!3m1!1e3")
+
+    #print([int(x) for x in path[4]])
+    interpolated_path = spline_interpolation(path, 35)
+
+    print("+++++++ Interpolated path +++++++")
+
+    for i in range(len(interpolated_path[0])):
+        print("https://www.google.ch/maps/" + point_to_string(path_at(interpolated_path, i)) + "t/data=!3m1!1e3")
+    #screenshot_url("https://www.google.ch/maps/" + point_to_string(path_at(interpolated_path, i)) + "t/data=!3m1!1e3", i+1)
+
+    #images_to_video('out/', '.png', 'path.mp4')
+
+#   print([int(x) for x in interpolated_path[4]])
+
+    import matplotlib.pyplot as plt
+    ax = plt.axes()
+    ax.plot(path[0], path[1], 'o', interpolated_path[0], interpolated_path[1], '-')
+
+    d_x = [sin(h * 2.0 * pi / 360 + pi / 4.0) * 0.003 for h in path[4]]
+    d_y = [cos(h * 2.0 * pi / 360 + pi / 4.0) * 0.003 for h in path[4]]
+
+    #for i in range(len(path[0])):
+    #   plt.arrow(path[0][i], path[1][i], d_x[i], d_y[i], fc="k", ec="k", head_width=0.001, head_length=0.001 )
+    #plt.arrow(interpolated_path[0][i], interpolated_path[1][i], d_x[i], d_y[i], fc="k", ec="k", head_width=0.00001, head_length=0.001 )
+
+
+    #ax.axis([48.84, 48.9, 2.28, 2.32])
+    #ax.arrow( 1.5, 0.8, 0.2, -0.2, fc="k", ec="k", head_width=0.05, head_length=0.1 )
+
+    plt.show()
+
+    plt.plot(path[5], path[4], 'o', interpolated_path[5], interpolated_path[4], '-', interpolated_path[5], interpolated_path[3], '--')
+
+    plt.show()
+    print(interpolated_path[4])
+    
 # Point {x, y, z}
 # CameraPoint {p, t, h}
--- path.py
+++ path.py
@@ -10,26 +10,26 @@ def coordinate_distance(lat1, lon1, lat2, lon2):
     return d * 1000.0 # meters
 
 def meter_to_coordinate(m_x, m_y, lat):
-	coord_x = m_x / 6378137.0
-	#coord_y = m_y / (6378137.0 * cos(lat / 360 * 2 * pi))
-	coord_y = m_y / (6378137.0 * cos(lat / 360.0 * 2 * pi))
-	return (coord_x, coord_y)
+    coord_x = m_x / 6378137.0
+    #coord_y = m_y / (6378137.0 * cos(lat / 360 * 2 * pi))
+    coord_y = m_y / (6378137.0 * cos(lat / 360.0 * 2 * pi))
+    return (coord_x, coord_y)
 
 # Return string for google maps address
 # Takes x and y coordinates, rayon of the rotation, tilt and revolution (between 0 and 1)
 def compute_camera_values(origin_x, origin_y, rayon_meter, tilt, rev):
-	
-	tilt_rayon_meter = rayon_meter * sin(tilt / 360.0 * 2.0 * pi)
+    
+    tilt_rayon_meter = rayon_meter * sin(tilt / 360.0 * 2.0 * pi)
 
-	(x_offset, y_offset) = meter_to_coordinate(tilt_rayon_meter * sin(rev * 2.0 * pi), tilt_rayon_meter * cos(rev * 2.0 * pi), origin_x)
+    (x_offset, y_offset) = meter_to_coordinate(tilt_rayon_meter * sin(rev * 2.0 * pi), tilt_rayon_meter * cos(rev * 2.0 * pi), origin_x)
 
-	x = origin_x + x_offset * 180 / pi
-	y = origin_y + y_offset * 180 / pi
+    x = origin_x + x_offset * 180 / pi
+    y = origin_y + y_offset * 180 / pi
 
-	#meter_rayon = coordinate_distance(origin_x, origin_y, x, y)
-	h = (360.0 - 90.0 - 360.0 * rev) % 360.0
-	altitude = rayon_meter * cos(tilt / 360.0 * 2.0 * pi)
-	return "@" + str(x) + "," + str(y) + "," + str(altitude) + "a," + "35y," + str(h) + "h,"+ str(tilt) + "t/data=!3m1!1e3"
+    #meter_rayon = coordinate_distance(origin_x, origin_y, x, y)
+    h = (360.0 - 90.0 - 360.0 * rev) % 360.0
+    altitude = rayon_meter * cos(tilt / 360.0 * 2.0 * pi)
+    return "@" + str(x) + "," + str(y) + "," + str(altitude) + "a," + "35y," + str(h) + "h,"+ str(tilt) + "t/data=!3m1!1e3"
 
 
 # tuple CamPoint {x, y, altitude, tilt, h, time}
@@ -40,132 +40,155 @@ import scipy as sp
 
 # Add full rotation around center
 def add_rotation(path, center_point, radius, tilt, N, time_beg, time_end, rot_beg = 0, rot_width = 360):
-	rad_tilt = tilt / 360.0 * 2.0 * pi
-	sin_radius = radius * sin(rad_tilt)
+    rad_tilt = tilt / 360.0 * 2.0 * pi
+    sin_radius = radius * sin(rad_tilt)
 
-	for i in range(int(N)):
-		rev = 2.0 * pi * (rot_beg + (rot_width) * float(i) / (N-1)) / 360
-		(x_offset, y_offset) = meter_to_coordinate(sin_radius * sin(rev), sin_radius * cos(rev), center_point[0])
-		x = center_point[0] + x_offset * 180 / pi
-		y = center_point[1] + y_offset * 180 / pi
-		altitude = center_point[2] + radius * cos(rad_tilt)
-		h = (360.0 - 90.0 - (rot_beg + (rot_width) * float(i) / (N-1)))
+    for i in range(int(N)):
+        rev = 2.0 * pi * (rot_beg + (rot_width) * float(i) / (N-1)) / 360
+        (x_offset, y_offset) = meter_to_coordinate(sin_radius * sin(rev), sin_radius * cos(rev), center_point[0])
+        x = center_point[0] + x_offset * 180 / pi
+        y = center_point[1] + y_offset * 180 / pi
+        altitude = center_point[2] + radius * cos(rad_tilt)
+        h = (360.0 - 90.0 - (rot_beg + (rot_width) * float(i) / (N-1)))
 
-		time = time_beg + (time_end - time_beg) * float(i) / N
+        time = time_beg + (time_end - time_beg) * float(i) / N
 
-		path[0].append(x)
-		path[1].append(y)
-		path[2].append(altitude)
-		path[3].append(tilt)
-		path[4].append(h)
-		path[5].append(time)
+        path[0].append(x)
+        path[1].append(y)
+        path[2].append(altitude)
+        path[3].append(tilt)
+        path[4].append(h)
+        path[5].append(time)
 
 # This doesn't add the first and the last one
 def add_traveling(path, p_a, p_b, N, time_beg, time_end, altitude_offset = 0):
-	d_x = p_b[0] - p_a[0] 
-	d_y = p_b[1] - p_a[1]
-	d_a = p_b[2] - p_a[2]
+    d_x = p_b[0] - p_a[0] 
+    d_y = p_b[1] - p_a[1]
+    d_a = p_b[2] - p_a[2]
 
-	h = (acos(d_x / sqrt(d_x * d_x + d_y * d_y)) / (2.0 * pi) * 360) % 360
-	if d_y < 0:
-		h = (h + 90) % 360
+    h = (acos(d_x / sqrt(d_x * d_x + d_y * d_y)) / (2.0 * pi) * 360) % 360
+    if d_y < 0:
+        h = (h + 90) % 360
 
-	for i in range(1, int(N)):
-		x = p_a[0] + d_x * float(i) / (N+1)
-		y = p_a[1] + d_y * float(i) / (N+1)
-		altitude = altitude_offset + p_a[2] + d_a * float(i) / (N+1)
+    for i in range(1, int(N)):
+        x = p_a[0] + d_x * float(i) / (N+1)
+        y = p_a[1] + d_y * float(i) / (N+1)
+        altitude = altitude_offset + p_a[2] + d_a * float(i) / (N+1)
 
-		time = time_beg + (time_end - time_beg) * float(i) / (N+1)
+        time = time_beg + (time_end - time_beg) * float(i) / (N+1)
 
-		path[0].append(x)
-		path[1].append(y)
-		path[2].append(altitude)
-		path[3].append(78.0)
-		path[4].append(h)		
-		path[5].append(time)
+        path[0].append(x)
+        path[1].append(y)
+        path[2].append(altitude)
+        path[3].append(78.0)
+        path[4].append(h)        
+        path[5].append(time)
 
 
 
 def path_at(path, i):
-	return (path[0][i], path[1][i], path[2][i], path[3][i], path[4][i], path[5][i])
+    return (path[0][i], path[1][i], path[2][i], path[3][i], path[4][i], path[5][i])
 
 def point_to_string(p):
-	return "@" + str(p[0]) + "," + str(p[1]) + "," + str(p[2]) + "a," + "35y," + str((p[4] % 360)) + "h,"+ str(p[3])
+    return "@" + str(p[0]) + "," + str(p[1]) + "," + str(p[2]) + "a," + "35y," + str((p[4] % 360)) + "h,"+ str(p[3])
 
 
 from scipy.interpolate import CubicSpline
 # return a list of camera point uniformly sample in time
 def spline_interpolation(path, nb_frame):
-	frame_rate = (path[5][-1] - path[5][0]) / (nb_frame-1)
+    frame_rate = (path[5][-1] - path[5][0]) / (nb_frame-1)
 
-	res_list = []
+    res_list = []
 
-	spline_x = CubicSpline(path[5], path[0])
-	spline_y = CubicSpline(path[5], path[1])
-	spline_a = CubicSpline(path[5], path[2])
-	spline_tilt = CubicSpline(path[5], path[3])
+    spline_x = CubicSpline(path[5], path[0])
+    spline_y = CubicSpline(path[5], path[1])
+    spline_a = CubicSpline(path[5], path[2])
+    spline_tilt = CubicSpline(path[5], path[3])
 
-	list_t = []
-	for i in range(int(nb_frame)):
-		list_t.append(frame_rate * i)
-	
-	x = spline_x(list_t)
-	y = spline_y(list_t)
-	a = spline_a(list_t)
-	tilt = spline_tilt(list_t)
+    list_t = []
+    for i in range(int(nb_frame)):
+        list_t.append(frame_rate * i)
+    
+    x = spline_x(list_t)
+    y = spline_y(list_t)
+    a = spline_a(list_t)
+    tilt = spline_tilt(list_t)
 
-	# make sure that h is monotically decreasing
+    # make sure that h is monotically decreasing
 
-	spline_h = CubicSpline(path[5], path[4])
-	h = spline_h(list_t)
-	h = [hh % 360 for hh in h]
-	time = list_t
-	return (x, y, a, tilt, h, time)
-
-	#	res_list
+    spline_h = CubicSpline(path[5], path[4])
+    h = spline_h(list_t)
+    h = [hh % 360 for hh in h]
+    time = list_t
+    return (x, y, a, tilt, h, time)
 
 
 # Get a list of destination (in the right order)
 # Will do a half-rotation around each destination
 # Need to first compute entry degree
 
-def plan_trip(list_places):
+def plan_trip(list_places, radius = 800, time_per_destination = 2.0):
 
-	path = [[],[],[],[],[],[]]
+    path = [[],[],[],[],[],[]]
 
-	# Compute direction between two destination.
-	# We want to end with it as the last direction for the rotation
+    # Compute direction between two destination.
+    # We want to end with it as the last direction for the rotation
 
-	# And start the next rotation with this direction
-	time = 0.0
-	current_h = 0
-	next_h = 0
-	for x in range(len(list_places)-1):
-		time_in = time
-		time_out = time + 2.0
+    # And start the next rotation with this direction
+    time = 0.0
+    current_h = 0
+    next_h = 0
+    for x in range(len(list_places)-1):
+        time_in = time
+        time_out = time + time_per_destination
 
-		# next_dir is the target dir for the end of this rotation
-		dx = list_places[x+1][0] - list_places[x][0]
-		dy = list_places[x+1][1] - list_places[x][1]
+        # next_dir is the target dir for the end of this rotation
+        dx = list_places[x+1][0] - list_places[x][0]
+        dy = list_places[x+1][1] - list_places[x][1]
 
-		h = (acos(dx / sqrt(dx * dx + dy * dy)) / (2.0 * pi) * 360) % 360
+        distance = sqrt(dx * dx + dy * dy)
 
-		# threshold so spline interpolate rotation during traveling as well
+        h = (acos(dx / distance) / (2.0 * pi) * 360) % 360
 
-		h = h - 15
+        # threshold so spline interpolate rotation during traveling as well
+        #h = h - 15
 
-		if x == 0:
-			current_h = ( h + 180 ) % 360
+        if x == 0:
+            current_h = ( h + 180 ) % 360
 
-		add_rotation(path, list_places[x], 1000, 40, 5, time_in, time_out, current_h, (h - current_h) % 360 )
+        add_rotation(path, list_places[x], radius, 40, 5, time_in, time_out, current_h, (h - current_h) % 360 )
 
-		time = time + 4.0
-		current_h = h
+        time_travel = distance / 0.02 * time_per_destination
 
-	time_in = time
-	time_out = time + 2.0
-	add_rotation(path, list_places[-1], 1000, 40, 5, time_in, time_out, current_h, 180 )
+        if x != 0:
+            # add a point in between the current destination and the next at higher altitude
+            # do this only if we travel a certain distance
+            distance_m = coordinate_distance(path[0][-6], path[1][-6], path[0][-5], path[1][-5])
+            path[0].insert(len(path[0]) - 5, (path[0][-6] + path[0][-5]) / 2.0)
+            path[1].insert(len(path[1]) - 5, (path[1][-6] + path[1][-5]) / 2.0)
+            path[2].insert(len(path[2]) - 5, (path[2][-6] + path[2][-5] + distance_m * 0.5) / 2.0)
+            path[3].insert(len(path[3]) - 5, 60.0)
+            path[4].insert(len(path[4]) - 5, path[4][-6])        
+            path[5].insert(len(path[5]) - 5, (path[5][-6] + path[5][-5]) / 2.0)
 
-	print(path[5])
+        current_h = h
+        time = time + time_per_destination + time_travel
 
-	return path
+
+    time_in = time
+    time_out = time + time_per_destination
+    add_rotation(path, list_places[-1], 1000, 40, 5, time_in, time_out, current_h, 180 )
+
+    # add a point in between the current destination and the next at higher altitude
+    # do this only if we travel a certain distance
+    distance = sqrt(dx * dx + dy * dy)
+    distance_m = coordinate_distance(path[0][-6], path[1][-6], path[0][-5], path[1][-5])
+    time_travel = distance / 0.02 * time_per_destination
+
+    path[0].insert(len(path[0]) - 5, (path[0][-6] + path[0][-5]) / 2.0)
+    path[1].insert(len(path[1]) - 5, (path[1][-6] + path[1][-5]) / 2.0)
+    path[2].insert(len(path[2]) - 5, (path[2][-6] + path[2][-5] + distance_m * 0.5) / 2.0)
+    path[3].insert(len(path[3]) - 5, 40.0)
+    path[4].insert(len(path[4]) - 5, path[4][-6])        
+    path[5].insert(len(path[5]) - 5, (path[5][-6] + path[5][-5]) / 2.0)
+    return path
