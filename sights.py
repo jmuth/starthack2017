4e39009c300974a25fe4efbce5d9feff43530a39
jmuth <joachim.h.muth@gmail.com>
2017-03-18 09:45:17 +0100

    pass name by parameters

--- sights.py
+++ sights.py
@@ -1,41 +1,38 @@
-import webbrowser
-from selenium import webdriver
-
-# webbrowser.open('http://www.sightsmap.com/')
-
-
-# elem = driver.find_element_by_id("searchfield")
-
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("http://www.sightsmap.com/")
-# assert "Python" in driver.title
-elem = driver.find_element_by_id("searchfield")
-elem.send_keys('London')
-elem.send_keys(Keys.RETURN)
-time.sleep(2)
-assert "No results found." not in driver.page_source
-num = (driver.execute_script("return mapMarkersArray.length"))
 
-markers = []
-for i in range(num):
+def getSights(name='Fribourg'):
+    print("Getting sights of:", name)
+    driver = webdriver.Chrome()
+    # enter website
+    driver.get("http://www.sightsmap.com/")
+    # search the site
+    elem = driver.find_element_by_id("searchfield")
+    elem.send_keys(name)
+    elem.send_keys(Keys.RETURN)
+    time.sleep(2)
 
-    request = "return mapMarkersArray[" + str(i) + "]"
-    title = request + ".marker.title"
-    lat = request + ".poi.lat"
-    lng = request + ".poi.lng"
+    #  get the displayed markers
+    num = (driver.execute_script("return mapMarkersArray.length"))
+    markers = []
+    for i in range(num):
+        request = "return mapMarkersArray[" + str(i) + "]"
+        title = request + ".marker.title"
+        lat = request + ".poi.lat"
+        lng = request + ".poi.lng"
 
-    marker = [driver.execute_script(title),
-              driver.execute_script(lat),
-              driver.execute_script(lng)
-              ]
+        marker = [driver.execute_script(title),
+                  driver.execute_script(lat),
+                  driver.execute_script(lng)
+                  ]
 
-    markers.append(marker)
+        markers.append(marker)
 
-print(markers)
+    # quit browser
+    driver.quit()
+    print("Success")
+    return markers
 
-driver.quit()
-# driver.close()
+    # driver.close()
